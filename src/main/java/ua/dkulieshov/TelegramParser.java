09b30562f56667d7fb09699d005014e57427dc10
Dmytro Kulieshov <pakirava.datsuma@gmail.com>
2022-08-13 21:54:36 +0300

    fix update_id parsing

--- src/main/java/ua/dkulieshov/TelegramParser.java
+++ src/main/java/ua/dkulieshov/TelegramParser.java
@@ -12,7 +12,7 @@ public class TelegramParser {
   public static final String WRAP_1_WITH_DELIMITERS = DELIMITER + "$1" + DELIMITER;
 
   public static Optional<String> parseUpdateOffset(String response) {
-    return extractFirstRegexGroup(response, RegexPattern.UPDATE_RESPONSE_WITH_1_OFFSET_GROUP_REGEX);
+    return extractFirstRegexGroup(response, RegexPattern.UPDATE_ID);
   }
 
   private static Optional<String> extractFirstRegexGroup(String response, RegexPattern regex) {
@@ -28,9 +28,8 @@ public class TelegramParser {
   }
 
   public static List<String> parseChatIds(String responseWithChatMessages) {
-    String[] parts = responseWithChatMessages.replaceAll(
-            RegexPattern.MESSAGES_RESPONSE_WITH_1_CHAT_ID_GROUP_REGEX.regex, WRAP_1_WITH_DELIMITERS)
-        .split(DELIMITER);
+    String[] parts = responseWithChatMessages.replaceAll(RegexPattern.CHAT_ID.regex,
+        WRAP_1_WITH_DELIMITERS).split(DELIMITER);
 
     List<String> list = new ArrayList<>();
     for (int i = 1; i < parts.length; i = i + 2) {
@@ -46,16 +45,16 @@ public class TelegramParser {
   }
 
   public static Optional<String> parseSentMessageId(String responseWithSentMessageId) {
-    return extractFirstRegexGroup(responseWithSentMessageId,
-        RegexPattern.SENT_MESSAGE_RESPONSE_WITH_1ST_MESSAGE_ID_GROUP_REGEX);
+    return extractFirstRegexGroup(responseWithSentMessageId, RegexPattern.MESSAGE_ID);
   }
 
   enum RegexPattern {
-    UPDATE_RESPONSE_WITH_1_OFFSET_GROUP_REGEX(
-        "\"update\":\\s+(\\d+),"), MESSAGES_RESPONSE_WITH_1_CHAT_ID_GROUP_REGEX(
-        "\"chat_id\":\\s+(\\d+),"), SENT_MESSAGE_RESPONSE_WITH_1ST_MESSAGE_ID_GROUP_REGEX(
-        "\"message_id\":\\s+(\\d+),"),
-    /*  */;
+    //@formatter:off
+    UPDATE_ID("\"update_id\":\\s*(\\d+),"),
+    CHAT_ID("\"chat_id\":\\s*(\\d+),"),
+    MESSAGE_ID("\"message_id\":\\s*(\\d+),"),
+    ;
+    //@formatter:on
     private final String regex;
 
     RegexPattern(String regex) {
