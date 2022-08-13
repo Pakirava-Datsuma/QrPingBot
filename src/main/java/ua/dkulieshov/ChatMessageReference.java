beac759715b9e9826429b97749ab2a0180a886c8
Dmytro Kulieshov <pakirava.datsuma@gmail.com>
2022-08-13 21:11:46 +0300

    add repetition number

--- src/main/java/ua/dkulieshov/ChatMessageReference.java
+++ src/main/java/ua/dkulieshov/ChatMessageReference.java
@@ -1,27 +1,51 @@
 package ua.dkulieshov;
 
-import java.util.Optional;
+import com.google.common.base.Preconditions;
 
 public class ChatMessageReference {
 
-  private Optional<String> id = Optional.empty();
-  private Optional<String> text = Optional.empty();
+  public static final ChatMessageReference EMPTY = new ChatMessageReference(null, null, true, 0);
+  private final String id;
+  private final String text;
+  private final boolean isEmpty;
 
-  public void update(String id, String text) {
-    this.id = Optional.of(id);
-    this.text = Optional.of(text);
+  private int repetitionNumber = 0;
+
+  public ChatMessageReference(String id, String text) {
+    this(id, text, false, 1);
   }
 
+  private ChatMessageReference(String id, String text, boolean isEmpty, int repetitionNumber) {
+    this.id = id;
+    this.text = text;
+    this.isEmpty = isEmpty;
+    this.repetitionNumber = repetitionNumber;
+  }
+
+
   public String getId() {
-    return id.get();
+    validateExists();
+    return id;
+  }
+
+  private void validateExists() {
+    Preconditions.checkArgument(!isEmpty);
   }
 
   public String getText() {
-    return text.get();
+    validateExists();
+    return text;
   }
 
   public boolean textEquals(String otherText) {
-    return text.isPresent() && text.get().equals(otherText);
-    return text.filter(otherText::equals).isPresent();
+    if (isEmpty) {
+      return false;
+    }
+    return text.equals(otherText);
   }
+
+  public int incrementAndGetRepetition() {
+    return ++repetitionNumber;
+  }
+
 }
