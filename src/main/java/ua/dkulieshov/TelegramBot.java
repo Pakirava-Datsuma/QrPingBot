bbeedbbc6399e6ada0e93fc079ca37d520d9551c
Dmytro Kulieshov <pakirava.datsuma@gmail.com>
2022-08-13 18:15:48 +0300

    migrate Bot.Param & Bot.Cmd onto enum

--- src/main/java/ua/dkulieshov/TelegramBot.java
+++ src/main/java/ua/dkulieshov/TelegramBot.java
@@ -1,5 +1,6 @@
 package ua.dkulieshov;
 
+import com.google.common.base.Preconditions;
 import java.util.Map;
 
 public class TelegramBot {
@@ -10,41 +11,52 @@ public class TelegramBot {
   private final String botToken;
 
   public TelegramBot(String botName, String botToken) {
-
     this.botName = botName;
     this.botToken = botToken;
     selfLinkPrefix = "https://t.me/" + botName;
     botCommandUrlPrefix = "https://api.telegram.org/bot" + botToken;
   }
 
-  String buildBotStartForKey(String key) {
-    return buildBotStartLink(Map.of(ParamName.START, key));
+  String buildCmdUrl(Cmd cmd) {
+    return UrlUtils.buildUrl(botCommandUrlPrefix + cmd.path, Map.of());
   }
 
-  private String buildBotStartLink(Map<String, String> paramMap) {
-    return selfLinkPrefix + UrlUtils.buildParametersSuffix(paramMap);
+  String buildCmdUrl(Cmd cmd, Map<Param, String> parameters) {
+    return UrlUtils.buildUrl(botCommandUrlPrefix + cmd.path, Map.of());
   }
 
-  String buildBotStartForGroup(String chatIdOrName) {
-    return buildBotStartLink(Map.of(ParamName.STARTGROUP, chatIdOrName));
+  String buildStartLinkWithKeyNGroup(String chatIdOrName, String key) {
+    return buildBotStartLink(Map.of(Param.STARTGROUP, chatIdOrName, Param.START, key));
   }
 
-  String buildBotStartForKeyNGroup(String chatIdOrName, String key) {
-    return buildBotStartLink(Map.of(ParamName.STARTGROUP, chatIdOrName, ParamName.START, key));
+  private String buildBotStartLink(Map<Param, String> paramMap) {
+    return selfLinkPrefix + Param.UTILS.buildTParametersSuffix(paramMap);
   }
 
-  static class ParamName {
+  enum Param {
+    START("start"), STARTGROUP("startgroup"), TEXT("text"), CHAT_ID("chat_id"), OFFSET("offset"),
+    ;
 
-    public static final String START = "start";
-    public static final String STARTGROUP = "startgroup";
-    public static final String TEXT = "text";
-    public static final String CHAT_ID = "chat_id";
-    static final String OFFSET = "offset";
+    public static final UrlUtils<Param> UTILS = new UrlUtils<>(p -> p.key);
+    private final String key;
+
+    Param(String key) {
+      this.key = key;
+    }
   }
 
-  public class CmdPath {
+  enum Cmd {
+    SEND_MESSAGE("/sendMessage"), GET_UPDATES("/getUpdates")
+    /*  */;
 
-    static final String SEND_MESSAGE = "/sendMessage";
-    static final String GET_UPDATES = "/getUpdates";
+    private static final String SLASH_PREFIX = "/";
+    private final String path;
+
+    Cmd(String path) {
+      this.path = path;
+      String slash = SLASH_PREFIX;
+      Preconditions.checkArgument(!path.endsWith(slash));
+      Preconditions.checkArgument(path.startsWith(slash));
+    }
   }
 }
