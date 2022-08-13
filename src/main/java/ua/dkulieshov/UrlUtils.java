fb0598afdc6c24cd230e5ed1d1242a51fcbfcafc
Dmytro Kulieshov <pakirava.datsuma@gmail.com>
2022-08-13 18:14:49 +0300

    customize UrlUtils

--- src/main/java/ua/dkulieshov/UrlUtils.java
+++ src/main/java/ua/dkulieshov/UrlUtils.java
@@ -3,9 +3,16 @@ package ua.dkulieshov;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 import java.util.Map;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
-public class UrlUtils {
+public class UrlUtils<T> {
+
+  private final Function<T, String> paramStringifier;
+
+  public UrlUtils(Function<T, String> paramStringifier) {
+    this.paramStringifier = paramStringifier;
+  }
 
   static String buildUrl(String command, Map<String, String> parameters) {
     String encodedParameters = buildParametersSuffix(parameters);
@@ -26,5 +33,16 @@ public class UrlUtils {
 
   private static String encodeValue(String value) {
     return URLEncoder.encode(value, StandardCharsets.UTF_8);
+  }
+
+  public String buildTParametersSuffix(Map<T, String> paramMap) {
+    Map<String, String> map = transformKeys(paramMap);
+    return buildParametersSuffix(map);
+  }
+
+  private Map<String, String> transformKeys(Map<T, String> paramMap) {
+    Map<String, String> map = paramMap.entrySet().stream()
+        .collect(Collectors.toMap(e -> paramStringifier.apply(e.getKey()), e -> e.getValue()));
+    return map;
   }
 }
