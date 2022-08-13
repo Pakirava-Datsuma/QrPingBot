71b5bb1c9f7a7880cfa2611be73d21e9fc2a02bd
Dmytro Kulieshov <pakirava.datsuma@gmail.com>
2022-08-13 17:59:16 +0300

    wrap responses in Optional

--- src/main/java/ua/dkulieshov/TelegramClient.java
+++ src/main/java/ua/dkulieshov/TelegramClient.java
@@ -1,6 +1,5 @@
 package ua.dkulieshov;
 
-import com.google.common.base.Preconditions;
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
@@ -9,55 +8,38 @@ import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodySubscribers;
 import java.nio.charset.StandardCharsets;
 import java.util.Map;
-import ua.dkulieshov.TelegramBot.CmdPath;
-import ua.dkulieshov.TelegramBot.ParamName;
+import java.util.Optional;
+import ua.dkulieshov.TelegramBot.Cmd;
+import ua.dkulieshov.TelegramBot.Param;
 
 public class TelegramClient {
 
   public static final HttpClient HTTP_CLIENT = HttpClient.newHttpClient();
-  public final String botCommandUrlPrefix;
+  private final TelegramBot bot;
 
-  public TelegramClient(String botToken) {
-
-    botCommandUrlPrefix = "https://api.telegram.org/bot" + botToken;
+  public TelegramClient(TelegramBot bot) {
+    this.bot = bot;
   }
 
-  public String getUpdates() throws IOException, InterruptedException {
-    return executeBotUrl(CmdPath.GET_UPDATES);
-  }
-
-  private String executeBotUrl(String command) throws IOException, InterruptedException {
-    String url = getBotUrlOfSlashCommand(command);
+  public Optional<String> getUpdates() {
+    String url = bot.buildCmdUrl(Cmd.GET_UPDATES);
     return executeUrl(url);
   }
 
-  private String getBotUrlOfSlashCommand(String slashCommand) {
-    String slash = "/";
-    Preconditions.checkArgument(!botCommandUrlPrefix.endsWith(slash));
-    Preconditions.checkArgument(slashCommand.startsWith(slash));
-    return botCommandUrlPrefix + slashCommand;
-  }
-
-  private static String executeUrl(String url) throws IOException, InterruptedException {
+  private static Optional<String> executeUrl(String url) {
     System.out.println();
     System.out.println(" < < < : " + url);
-    HttpResponse<String> response = HTTP_CLIENT.send(
-        HttpRequest.newBuilder().GET().uri(URI.create(url)).build(),
-        responseInfo -> BodySubscribers.ofString(StandardCharsets.UTF_8));
+    HttpResponse<String> response = null;
+    try {
+      response = HTTP_CLIENT.send(HttpRequest.newBuilder().GET().uri(URI.create(url)).build(),
+          responseInfo -> BodySubscribers.ofString(StandardCharsets.UTF_8));
+      System.out.println(" > > > : " + response.toString());
+      System.out.println(" > > > : " + response.body());
+    } catch (IOException | InterruptedException e) {
+      e.printStackTrace();
+    }
 
-    System.out.println(" > > > : " + response.toString());
-    System.out.println(" > > > : " + response.body());
-
-    return response.body();
-  }
-
-  public String getUpdates(String offset) throws IOException, InterruptedException {
-    return executeBotUrl(CmdPath.GET_UPDATES, Map.of(ParamName.OFFSET, offset));
-  }
-
-  private String executeBotUrl(String command, Map<String, String> parameters)
-      throws IOException, InterruptedException {
-    return executeBotUrl(UrlUtils.buildUrl(command, parameters));
+    return Optional.ofNullable(response).map(HttpResponse::body);
   }
 
   /*{
@@ -77,9 +59,15 @@ public class TelegramClient {
     ]
   }
   */
-  public void send(String chatId, String message) throws IOException, InterruptedException {
 
-    executeBotUrl(CmdPath.SEND_MESSAGE, Map.of(ParamName.CHAT_ID, chatId, ParamName.TEXT, message));
+  public Optional<String> getUpdates(String offset) {
+    String url = bot.buildCmdUrl(Cmd.GET_UPDATES, Map.of(Param.OFFSET, offset));
+    return executeUrl(url);
   }
 
+  public void send(String chatId, String message) {
+    String url = bot.buildCmdUrl(Cmd.SEND_MESSAGE,
+        Map.of(Param.CHAT_ID, chatId, Param.TEXT, message));
+    executeUrl(url);
+  }
 }
