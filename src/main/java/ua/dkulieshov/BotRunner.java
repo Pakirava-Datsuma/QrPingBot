a69e680ebeaeba144f75d9c35f2e06f3c8e58d4e
Dmytro Kulieshov <pakirava.datsuma@gmail.com>
2022-08-17 23:21:48 +0300

    add TODO

--- src/main/java/ua/dkulieshov/BotRunner.java
+++ src/main/java/ua/dkulieshov/BotRunner.java
@@ -10,11 +10,7 @@ public class BotRunner {
     TelegramClient client = new TelegramClient(botId);
     Chat adminChat = new Chat(BotLoader.ADMIN_CHAT_ID, client);
 
-    String link = botId.buildPingLink(BotLoader.ADMIN_CHAT_ID);
-    InputStream qrCodeImageStream = QrGenerator.generateQRCodeImage(link);
-    adminChat.sendQrCode(qrCodeImageStream);
-
-    //    System.exit(0);
+    sendQrCode(botId, adminChat); // TODO: send qr on "/start"
 
     String offset = Chat.waitFirstUpdate(client);
     adminChat.sendRepeatableMessage("Last offset found: " + offset);
@@ -22,6 +18,9 @@ public class BotRunner {
     while (true) {
       offset = String.valueOf(Long.parseLong(offset) + 1);
 
+      // TODO: get @from and:
+      // TODO: 1. send "sent" back to @from
+      // TODO: 2. send "@from" to chatId
       List<String> pingChatIds = Chat.waitPingChatIds(client, offset);
 
       for (String pingChatId : pingChatIds) {
@@ -31,6 +30,12 @@ public class BotRunner {
 
       adminChat.sendRepeatableMessage("No!");
     }
+  }
+
+  private static void sendQrCode(BotId botId, Chat adminChat) throws Exception {
+    String link = botId.buildPingLink(BotLoader.ADMIN_CHAT_ID);
+    InputStream qrCodeImageStream = QrGenerator.generateQRCodeImage(link);
+    adminChat.sendQrCode(qrCodeImageStream);
   }
 
 
